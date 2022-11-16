7a83633ecff3eea0d017188efc902eb57814b10d
Eugene <evsyrovatka@gmail.com>
2022-11-17 00:12:49 +0200

    add selected cell

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, FC } from "react";
 import { Board } from "../models/Board";
+import { Cell } from "../models/Cell";
 import CellComponent from "./CellComponent";
 
 interface BoardProps {
@@ -7,13 +8,26 @@ interface BoardProps {
   setBoard: (board: Board) => void;
 }
 
-const BoardComponent: React.FC<BoardProps> = ({ board, setBoard }) => {
+const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  const handler = (cell: Cell) => {
+    setSelectedCell(cell);
+  };
+
   return (
     <div className="board">
       {board.cells.map((row, index) => (
         <React.Fragment key={index}>
           {row.map((cell) => (
-            <CellComponent cell={cell} key={cell.id} />
+            <CellComponent
+              cell={cell}
+              key={cell.id}
+              selected={
+                cell.x === selectedCell?.x && cell.y === selectedCell?.y
+              }
+              click={handler}
+            />
           ))}
         </React.Fragment>
       ))}
