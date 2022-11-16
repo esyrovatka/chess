7bed287f0c6beb7b69600ccd90ef35d36f87906e
Eugene <evsyrovatka@gmail.com>
2022-11-16 23:31:48 +0200

    create models, create template board, sell

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import BoardComponent from "./components/BoardComponent";
+import { Board } from "./models/Board";
 
 function App() {
+  const [board, setBoard] = useState(new Board());
+
+  const restart = () => {
+    const newBoard = new Board();
+    newBoard.initCells();
+    setBoard(newBoard);
+  };
+
+  useEffect(() => {
+    restart();
+  }, []);
+
   return (
     <div className="App">
-    
+      <BoardComponent board={board} setBoard={setBoard} />
     </div>
   );
 }
