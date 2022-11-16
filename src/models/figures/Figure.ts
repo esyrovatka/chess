8284889d4c98c592fdff639513ec32088b4ea44a
Eugene <evsyrovatka@gmail.com>
2022-11-16 23:51:09 +0200

    add figures models

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,1 +1,37 @@
-export class Figure {}
+import logo from "../../assets/black-bishop.png";
+import { Cell } from "../Cell";
+
+import { Colors } from "../Colors";
+
+export enum FigureName {
+  FIGURE = "фигура",
+  KING = "король",
+  KNIGHT = "Конь",
+  PAWN = "пешка",
+  QUEEN = "Ферзь",
+  ROOK = "Ладья",
+  BISHOP = "Слон",
+}
+
+export class Figure {
+  color: Colors;
+  logo: typeof logo | null;
+  cell: Cell;
+  name: FigureName;
+  id: number;
+
+  constructor(color: Colors, cell: Cell) {
+    this.color = color;
+    this.cell = cell;
+    this.cell.figure = this;
+    this.logo = null;
+    this.name = FigureName.FIGURE;
+    this.id = Math.random();
+  }
+
+  canMove(target: Cell): boolean {
+    return true;
+  }
+
+  moveFigure(target: Cell) {}
+}
